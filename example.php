e5cabba06a343958a607678f43669fecdc0e1cfb
meldon <tfo2011@gmail.com>
2017-03-02 15:44:33 +0300

    init

--- example.php
+++ example.php
@@ -1,15 +1,15 @@
 <?php
 require_once "HtmlCleaner.php";
 $cleaner = new HtmlCleaner();
-//$habra = file_get_contents("letters/geektimes.html");
-//file_put_contents("result/geektimes.html",$cleaner->clean($habra));
-//
-//$habra = file_get_contents("letters/redmine.html");
-//file_put_contents("result/redmine.html",$cleaner->clean($habra));
-//
-//
-//$habra = file_get_contents("letters/other_two.html");
-//file_put_contents("result/other_two.html",$cleaner->clean($habra));
+$habra = file_get_contents("letters/geektimes.html");
+file_put_contents("result/geektimes.html",$cleaner->clean($habra));
+
+$habra = file_get_contents("letters/redmine.html");
+file_put_contents("result/redmine.html",$cleaner->clean($habra));
+
+
+$habra = file_get_contents("letters/other_two.html");
+file_put_contents("result/other_two.html",$cleaner->clean($habra));
 //
 
 
